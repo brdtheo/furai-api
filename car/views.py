952bcf14b878df3660ca14f6426a4ee99cc14c69
Theo Billardey <toto.billardey@gmail.com>
2025-05-15 19:58:26 +0200

    feat(car): create details page

--- car/views.py
+++ car/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 
 def car_list(request: HttpRequest) -> HttpResponse:
-    return render(request,"list.html")
+    return render(request, "list.html")
+
+
+def car_details(request: HttpRequest, car_slug: str) -> HttpResponse:
+    car_name = "Honda Civic 1.6 Vti"
+    return render(request, "details.html", context={"car_name": car_name})

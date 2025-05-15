f983f24cc9758586ba11653ddabdd6de2e936397
Theo Billardey <toto.billardey@gmail.com>
2025-05-15 19:58:26 +0200

    feat(car): create details page

--- car/urls.py
+++ car/urls.py
@@ -1,7 +1,8 @@
 from django.urls import URLPattern, URLResolver, path
 
-from .views import car_list
+from .views import car_details, car_list
 
 urlpatterns: list[URLResolver | URLPattern] = [
     path("", car_list, name="car-list"),
+    path("<str:car_slug>", car_details, name="car-details"),
 ]

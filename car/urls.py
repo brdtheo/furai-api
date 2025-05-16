5b305f135c1856313e8cb0cd9502e0dc567eb8f8
Theo Billardey <toto.billardey@gmail.com>
2025-05-16 18:23:06 +0200

    feat(car): remove car list view

--- car/urls.py
+++ car/urls.py
@@ -1,8 +1,7 @@
 from django.urls import URLPattern, URLResolver, path
 
-from .views import car_details, car_list
+from .views import car_details
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("", car_list, name="car-list"),
     path("<str:car_slug>", car_details, name="car-details"),
 ]

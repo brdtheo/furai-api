e6c08422eaac3208c0e3444869fe2b2a79c400d2
Theo Billardey <toto.billardey@gmail.com>
2025-05-19 18:03:07 +0200

    refactor(templates): make hero section generic

--- furai/views.py
+++ furai/views.py
@@ -5,8 +5,14 @@ from car.models import Car, CarMedia
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    car_list = Car.objects.all().order_by("price_24_hours_cents")[:10]
+    hero_buttons = [
+        {"label": "our fleet", "href": "#fleet"},
+        {"label": "line", "href": "#contact"},
+        {"label": "whatsapp", "href": "#contact"},
+    ]
+    hero_thumbnail = "https://www.jcwhitney.com/wp-content/uploads/2024/08/Screenshot-at-Aug-13-13-25-42-1500x442.webp"
 
+    car_list = Car.objects.all().order_by("price_24_hours_cents")[:10]
     car_with_media_list = []
     for car in car_list:
         car_media_thumbnail = CarMedia.objects.get(car=car, is_thumbnail=True)
@@ -22,5 +28,9 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(
         request,
         "index.html",
-        context={"car_list": car_with_media_list},
+        context={
+            "car_list": car_with_media_list,
+            "hero_buttons": hero_buttons,
+            "hero_thumbnail": hero_thumbnail,
+        },
     )

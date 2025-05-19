a1f508b788b6e4b1e39612089776a33e9d1cd13b
Theo Billardey <toto.billardey@gmail.com>
2025-05-19 17:26:10 +0200

    feat(templates): use DB objects for fleet section

--- furai/views.py
+++ furai/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from car.models import Car, CarMedia
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
+    car_list = Car.objects.all().order_by("price_24_hours_cents")[:10]
+
+    car_with_media_list = []
+    for car in car_list:
+        car_media_thumbnail = CarMedia.objects.get(car=car, is_thumbnail=True)
+        car_with_media_list.append(
+            {
+                "slug": car.slug,
+                "name": car.name,
+                "transmission": car.transmission,
+                "thumbnail": car_media_thumbnail.url,
+            }
+        )
+
+    return render(
+        request,
+        "index.html",
+        context={"car_list": car_with_media_list},
+    )

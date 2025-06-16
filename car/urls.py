10fb644ab45f8f236e7c167980a52fee3f54203f
Theo Billardey <toto.billardey@gmail.com>
2025-06-16 17:04:35 +0200

    refactor(urls): setup API urls
    Replace template urls by API urls

--- car/urls.py
+++ car/urls.py
@@ -1,7 +1,8 @@
 from django.urls import URLPattern, URLResolver, path
 
-from .views import CarDetailView
+from .views import car_list, car_media_list
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("<str:car_slug>", CarDetailView.as_view(), name="car-details"),
+    path("cars/", car_list),
+    path("car-medias/", car_media_list),
 ]

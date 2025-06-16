f13709458949aa3f188ccaf5fbbf24cc6ddf3bf4
Theo Billardey <toto.billardey@gmail.com>
2025-06-16 16:25:09 +0200

    GCP serializers

--- car/serializers.py
+++ car/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Car
+from .models import Car, CarMedia
 
 
 class CarSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField()
+
     class Meta:
         model = Car
+        fields = "__all__"
+
+
+class CarMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarMedia
         fields = "__all__"

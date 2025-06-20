9545045d8e3124f9b6c6bbed6c51f8bb764bb6ba
Theo Billardey <toto.billardey@gmail.com>
2025-06-20 17:07:32 +0200

    refactor(car): switch to class based views
    Import generic views and use DRF mixins to replace function based views

--- car/urls.py
+++ car/urls.py
@@ -1,10 +1,10 @@
 from django.urls import URLPattern, URLResolver, path
 
-from .views import car_details, car_feature_list, car_list, car_media_list
+from .views import CarDetail, CarFeatureList, CarList, CarMediaList
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("cars/", car_list),
-    path("cars/<int:id>/", car_details),
-    path("car-medias/", car_media_list),
-    path("car-features/", car_feature_list),
+    path("cars", CarList.as_view()),
+    path("cars/<int:pk>", CarDetail.as_view()),
+    path("car-medias", CarMediaList.as_view()),
+    path("car-features", CarFeatureList.as_view()),
 ]

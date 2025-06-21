4c22cfd43c3e306d0e84751a669e11487feac3e0
Theo Billardey <toto.billardey@gmail.com>
2025-06-21 16:24:44 +0200

    feat(booking): register model in admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("customer", "car", "start_date", "end_date", "status")
+    search_fields = (
+        "first_name",
+        "last_name",
+    )
+    list_filter = ("customer", "car")
+    list_per_page = 30

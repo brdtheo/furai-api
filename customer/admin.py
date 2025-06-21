d395a6ca3f6bf5dc6d4dfb2b55576e36c524334e
Theo Billardey <toto.billardey@gmail.com>
2025-06-21 16:04:13 +0200

    feat(customer): register model in admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Booking
+
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("name", "address_country", "booking_count")
+    search_fields = (
+        "first_name",
+        "last_name",
+    )
+    list_filter = ("address_country",)
+    list_per_page = 30
+
+    @admin.display(description="Name")
+    def name(self, obj: Customer) -> str:
+        return f"{obj.first_name} {obj.last_name}"
+
+    @admin.display(description="Booking count")
+    def booking_count(self, obj: Customer) -> int:
+        return Booking.objects.filter(customer=obj).count()

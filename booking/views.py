869b220001db53a2a738c46c1b77df570f48a6ce
Theo Billardey <toto.billardey@gmail.com>
2025-06-21 19:04:27 +0200

    feat(booking): create booking list view

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from rest_framework.generics import ListAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .models import Booking
+from .serializers import BookingSerializer
+
+
+class BookingList(ListAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        customer_id = request.GET["customer"] if request.GET else None
+        if customer_id:
+            self.queryset = Booking.objects.filter(customer_id=customer_id)
+        return self.list(request, *args, **kwargs)

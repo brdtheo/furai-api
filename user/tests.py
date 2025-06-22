2bad55bf0642d63e21f0c046692fe9d32f9c6525
Theo Billardey <toto.billardey@gmail.com>
2025-06-22 20:07:23 +0200

    test(user): use faker data

--- user/tests.py
+++ user/tests.py
@@ -1,14 +1,19 @@
 from django.forms import ValidationError
 from django.test import TestCase
+from faker import Faker
 
 from .models import CustomUser
+
+fake = Faker()
 
 
 class CustomUserTestCase(TestCase):
     def test_create_user(self):
         """Correctly creates a regular user"""
-        CustomUser.objects.create(email="a@a.fr")
-        user = CustomUser.objects.get(email="a@a.fr")
+
+        user_email = fake.email()
+        CustomUser.objects.create(email=user_email)
+        user = CustomUser.objects.get(email=user_email)
         assert user is not None
         assert user.email is not None
         assert not user.password
@@ -18,18 +23,22 @@ class CustomUserTestCase(TestCase):
 
     def test_fail_create_user_missing_email(self):
         """Fails to create a regular user if email is not provided"""
+
         with self.assertRaises(TypeError):
             CustomUser.objects.create_user()
 
     def test_fail_create_user_with_password(self):
         """Fails to create a regular user if a password is provided"""
+
         with self.assertRaises(ValidationError):
-            CustomUser.objects.create_user(email="a@a.fr", password="foo")
+            CustomUser.objects.create_user(email=fake.email(), password=fake.password())
 
     def test_create_superuser(self):
         """Correctly creates a super user"""
-        CustomUser.objects.create_superuser(email="a@a.fr", password="foo")
-        user = CustomUser.objects.get(email="a@a.fr")
+
+        user_email = fake.email()
+        CustomUser.objects.create_superuser(email=user_email, password=fake.password())
+        user = CustomUser.objects.get(email=user_email)
         assert user is not None
         assert user.email is not None
         assert user.password is not None

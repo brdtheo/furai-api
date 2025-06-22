20212b26195ccf76d0a0b7d650129bca01312942
Theo Billardey <toto.billardey@gmail.com>
2025-06-22 16:52:10 +0200

    test(booking): create base tests

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from datetime import datetime, timedelta, timezone
 
-# Create your tests here.
+from django.forms import ValidationError
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone as django_timezone
+from rest_framework.status import HTTP_200_OK
+from rest_framework.test import APITestCase
+
+from car.tests import set_up_car
+from customer.tests import set_up_customer
+
+from .enums import BookingStatus
+from .models import Booking
+
+
+def set_up_booking(car, customer):
+    """Creates a Booking instance in the test DB"""
+    booking = Booking.objects.create(
+        car=car,
+        customer=customer,
+        start_date=datetime(year=2026, month=6, day=17, hour=13, tzinfo=timezone.utc),
+        end_date=datetime(year=2026, month=6, day=17, hour=19, tzinfo=timezone.utc),
+        status=BookingStatus.CONFIRMED,
+    )
+    return booking
+
+
+class BookingTestCase(TestCase):
+    def setUp(self):
+        car = set_up_car()
+        customer = set_up_customer()
+        booking = set_up_booking(car, customer)
+        self.car = car
+        self.customer = customer
+        self.booking = booking
+
+    def test_create_booking_start_date_past(self):
+        """Ensures a booking cannot be created if start date is in the past"""
+
+        with self.assertRaises(ValidationError):
+            start_date = django_timezone.now() - timedelta(days=2)
+            Booking.objects.create(
+                car=self.car,
+                customer=self.customer,
+                start_date=start_date,
+                end_date=datetime(year=2026, month=6, day=17, tzinfo=timezone.utc),
+                status=BookingStatus.CONFIRMED,
+            )
+
+    def test_create_booking_start_date_same_day(self):
+        """Ensures a booking cannot be created if start date is current day"""
+
+        with self.assertRaises(ValidationError):
+            start_date = django_timezone.now()
+            Booking.objects.create(
+                car=self.car,
+                customer=self.customer,
+                start_date=start_date,
+                end_date=datetime(year=2026, month=6, day=17),
+                status=BookingStatus.CONFIRMED,
+            )
+
+    def test_update_booking_status(self):
+        """Ensures a booking status can be updated correctly"""
+
+        self.booking.status = BookingStatus.COMPLETED
+        self.booking.save()
+        assert self.booking.status == BookingStatus.COMPLETED
+
+
+class BookingAPITestCase(APITestCase):
+    def setUp(self):
+        car = set_up_car()
+        customer = set_up_customer()
+        booking = set_up_booking(car, customer)
+        self.car = car
+        self.customer = customer
+        self.booking = booking
+
+    def test_get_booking_list(self):
+        """Correctly list all bookings correctly"""
+
+        url = reverse("booking-list")
+        response = self.client.get(url, format="json")
+        assert response.status_code == HTTP_200_OK

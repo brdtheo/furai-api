b843b60a6bbc0c86d00a9d10558e3134467eaf4e
Theo Billardey <toto.billardey@gmail.com>
2025-07-01 17:44:39 +0200

    feat(car): set name for url paths

--- car/urls.py
+++ car/urls.py
@@ -3,8 +3,8 @@ from django.urls import URLPattern, URLResolver, path
 from .views import CarDetail, CarFeatureList, CarList, CarMediaList
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("cars", CarList.as_view()),
-    path("cars/<int:pk>", CarDetail.as_view()),
-    path("car-medias", CarMediaList.as_view()),
-    path("car-features", CarFeatureList.as_view()),
+    path("cars", CarList.as_view(), name="car-list"),
+    path("cars/<int:pk>", CarDetail.as_view(), name="car-detail"),
+    path("car-medias", CarMediaList.as_view(), name="car-media-list"),
+    path("car-features", CarFeatureList.as_view(), name="car-feature-list"),
 ]

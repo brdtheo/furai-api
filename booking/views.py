b8a05d3c9297a0198520f3c886c285f421798957
Theo Billardey <toto.billardey@gmail.com>
2025-07-01 17:43:26 +0200

    fix(booking): order querysets by descending created_at

--- booking/views.py
+++ booking/views.py
@@ -9,11 +9,11 @@ from .serializers import BookingSerializer
 
 
 class BookingList(ListAPIView):
-    queryset = Booking.objects.all()
+    queryset = Booking.objects.order_by("-created_at")
     serializer_class = BookingSerializer
 
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         customer_id = request.GET["customer"] if request.GET else None
         if customer_id:
-            self.queryset = Booking.objects.filter(customer_id=customer_id)
+            self.queryset = Booking.objects.filter(customer_id=customer_id).order_by("-created_at")
         return self.list(request, *args, **kwargs)

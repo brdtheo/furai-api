9e8f54d7a281a058aa31717831ce1e7ba9e467b9
Theo Billardey <toto.billardey@gmail.com>
2025-07-03 21:15:58 +0200

    feat(customer): retrieve current customer
    Create a new view to retrieve the current customer instance from authentication headers

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CustomerDetail
+from .views import CustomerDetail, CustomerMe
 
 urlpatterns = [
-    path("customers/<int:pk>", CustomerDetail.as_view(), name="customer-detail")
+    path("customers/<int:pk>", CustomerDetail.as_view(), name="customer-detail"),
+    path("customers/me", CustomerMe.as_view(), name="customer-me"),
 ]

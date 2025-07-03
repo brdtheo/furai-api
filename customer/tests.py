dd2b6ab58aa5b1ef4241aaec010fe6bb2d214d67
Theo Billardey <toto.billardey@gmail.com>
2025-07-03 21:15:58 +0200

    feat(customer): retrieve current customer
    Create a new view to retrieve the current customer instance from authentication headers

--- customer/tests.py
+++ customer/tests.py
@@ -3,9 +3,10 @@ from django.forms import ValidationError
 from django.test import TestCase
 from django.urls import reverse
 from faker import Faker
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 from rest_framework.test import APITestCase
 
+from furai.tests.utils import TestClientAuthenticator
 from user.models import CustomUser
 
 from .models import Customer
@@ -14,7 +15,7 @@ fake = Faker()
 
 
 def set_up_customer():
-    """Creates a User and Cusomer instance in the test DB"""
+    """Creates a CustomUser and Customer instance in the test DB"""
 
     first_name = fake.first_name()
     last_name = fake.last_name()
@@ -35,10 +36,38 @@ def set_up_customer():
     return customer
 
 
+def set_up_customer_list():
+    """Creates several random CustomUser and related Customer instances in the test DB"""
+
+    user_list = []
+    for i in range(10):
+        user = CustomUser.objects.create(
+            email=f"{fake.first_name()}.{fake.last_name()}@{fake.domain_name()}"
+        )
+        user_list.append(user)
+    customer_list = []
+    for user in user_list:
+        customer = Customer.objects.create(
+            user=user,
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            address_line1=fake.street_address(),
+            address_city=fake.city(),
+            address_postal_code=fake.postalcode(),
+            address_state=fake.state(),
+            address_country="US",
+            phone=fake.phone_number(),
+            passport=fake.passport_number(),
+        )
+        customer_list.append(customer)
+    return customer_list
+
+
 class CustomerTestCase(TestCase):
     def setUp(self):
         customer = set_up_customer()
         self.customer = customer
+        self.user = customer.user
 
     def test_create_customer_no_user(self):
         """Ensures a customer cannot be created without a linked user"""
@@ -78,6 +107,7 @@ class CustomerAPITestCase(APITestCase):
     def setUp(self):
         customer = set_up_customer()
         self.customer = customer
+        self.user = customer.user
 
     def test_retrieve_customer(self):
         """Correctly retrieves a customer instance"""
@@ -95,3 +125,20 @@ class CustomerAPITestCase(APITestCase):
         assert response.data["address_country"] == self.customer.address_country
         assert response.data["phone"] == self.customer.phone
         assert response.data["passport"] == self.customer.passport
+
+    def test_retrieve_current_customer(self):
+        """Correctly retrieves the current customer instance"""
+
+        TestClientAuthenticator.authenticate(self.client, self.user)
+        url = reverse("customer-me")
+        response = self.client.get(url, format="json")
+        assert response.status_code == HTTP_200_OK
+        assert response.data["user"] == self.user.id
+        TestClientAuthenticator.authenticate_logout(self.client)
+
+    def test_retrieve_current_customer_anonymous_user(self):
+        """Prevent retrieving the current customer if not authenticated"""
+
+        url = reverse("customer-me")
+        response = self.client.get(url, format="json")
+        assert response.status_code == HTTP_401_UNAUTHORIZED

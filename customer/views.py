a90e425986215a56214a7fdbbd55fc2e6843f5cd
Theo Billardey <toto.billardey@gmail.com>
2025-07-03 21:15:58 +0200

    feat(customer): retrieve current customer
    Create a new view to retrieve the current customer instance from authentication headers

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 from rest_framework.generics import RetrieveAPIView
 
 from .models import Customer
@@ -11,3 +13,16 @@ class CustomerDetail(RetrieveAPIView):
 
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+class CustomerMe(RetrieveAPIView):
+    """
+    Retrieve the customer instance from current authenticated user
+    """
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self) -> Customer:
+        return get_object_or_404(Customer, user=self.request.user)

13432b12cb70f108177878dc74fc5c9bfeb03e46
Theo Billardey <toto.billardey@gmail.com>
2025-07-03 21:17:01 +0200

    feat(booking): handle customer bookings in list view

--- booking/views.py
+++ booking/views.py
@@ -4,6 +4,9 @@ from rest_framework.generics import ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from customer.models import Customer
+from user.models import CustomUser
+
 from .models import Booking
 from .serializers import BookingSerializer
 
@@ -15,5 +18,12 @@ class BookingList(ListAPIView):
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         customer_id = request.GET["customer"] if request.GET else None
         if customer_id:
-            self.queryset = Booking.objects.filter(customer_id=customer_id).order_by("-created_at")
+            self.queryset = Booking.objects.filter(customer_id=customer_id).order_by(
+                "-created_at"
+            )
+        if isinstance(self.request.user, CustomUser):
+            customer = Customer.objects.get(user=self.request.user)
+            self.queryset = Booking.objects.filter(customer=customer).order_by(
+                "-created_at"
+            )
         return self.list(request, *args, **kwargs)

da881ec94d5eca8aa56da747297c6ded8db400d6
Theo Billardey <toto.billardey@gmail.com>
2025-07-07 18:04:50 +0200

    refactor(booking): better handling of current user for list view

--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,9 @@
-from typing import Any
+from typing import cast
 
+from django.db.models.query import QuerySet
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import Request
-from rest_framework.response import Response
 
-from customer.models import Customer
 from user.models import CustomUser
 
 from .models import Booking
@@ -13,19 +11,13 @@ from .serializers import BookingSerializer
 
 
 class BookingList(ListAPIView):
-    queryset = Booking.objects.order_by("-created_at")
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        customer_id = request.GET["customer"] if request.GET else None
-        if customer_id:
-            self.queryset = Booking.objects.filter(customer_id=customer_id).order_by(
-                "-created_at"
-            )
-        if isinstance(self.request.user, CustomUser):
-            customer = Customer.objects.get(user=self.request.user)
-            self.queryset = Booking.objects.filter(customer=customer).order_by(
-                "-created_at"
-            )
-        return self.list(request, *args, **kwargs)
+    def get_queryset(self) -> QuerySet[Booking]:
+        user: CustomUser = cast(
+            CustomUser,
+            self.request.user,
+        )
+        queryset = Booking.objects.filter(customer__user=user).order_by("-created_at")
+        return queryset

8f53e4eb7737495a73e9fa0efa30cbc040a1f608
Theo Billardey <toto.billardey@gmail.com>
2025-07-07 17:59:49 +0200

    feat(booking): use authenticated permission for list view

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -14,6 +15,7 @@ from .serializers import BookingSerializer
 class BookingList(ListAPIView):
     queryset = Booking.objects.order_by("-created_at")
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         customer_id = request.GET["customer"] if request.GET else None

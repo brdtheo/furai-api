a013f55f8309e5d82151d482da0b1064b73d74aa
Theo Billardey <toto.billardey@gmail.com>
2025-07-10 20:03:17 +0200

    feat(customer): update customer infos
    Import generic update api view from rest framework and allow patch/put requests

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CustomerMe
+from .views import CustomerDetailView, CustomerMe
 
 urlpatterns = [
+    path("customers/<int:pk>", CustomerDetailView.as_view(), name="customer-detail"),
     path("customers/me", CustomerMe.as_view(), name="customer-me"),
 ]

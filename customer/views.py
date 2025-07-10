a936dc3ad2cec930f6bd75cb72af83458530f69c
Theo Billardey <toto.billardey@gmail.com>
2025-07-10 20:03:17 +0200

    feat(customer): update customer infos
    Import generic update api view from rest framework and allow patch/put requests

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Customer
+from .permissions import IsCustomerUser
 from .serializers import CustomerSerializer
 
 
@@ -11,9 +12,18 @@ class CustomerMe(RetrieveAPIView):
     Retrieve the customer instance from current authenticated user
     """
 
-    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self) -> Customer:
         return get_object_or_404(Customer, user=self.request.user)
+
+
+class CustomerDetailView(UpdateAPIView):
+    """
+    Update a given customer instance
+    """
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsCustomerUser]

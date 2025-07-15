9873b1ddb9911e24fd22995ba0510bf697ab0765
Theo Billardey <toto.billardey@gmail.com>
2025-07-15 19:30:32 +0200

    feat(booking): cancel a booking
    Booking cancellation from customer or staff

--- booking/admin.py
+++ booking/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
-from .enums import BookingStatus
 from .models import Booking
 
 
-@admin.action(description="Mark bookings as complete")
-def mark_as_complete(modeladmin, request, queryset: QuerySet[Booking]):
-    queryset.update(status=BookingStatus.COMPLETED)
+@admin.action(description="Cancel selected bookings")
+def cancel(
+    modeladmin: admin.ModelAdmin,
+    request: HttpRequest,
+    queryset: QuerySet[Booking],
+) -> None:
+    for booking in queryset.all():
+        booking.cancel(True)
+
+
+@admin.action(description="Mark selected bookings as complete")
+def mark_as_complete(
+    modeladmin: admin.ModelAdmin,
+    request: HttpRequest,
+    queryset: QuerySet[Booking],
+) -> None:
+    for booking in queryset.all():
+        booking.mark_as_complete()
 
 
 @admin.register(Booking)
@@ -19,4 +34,11 @@ class BookingAdmin(admin.ModelAdmin):
     )
     list_filter = ("customer", "car")
     list_per_page = 30
-    actions = [mark_as_complete]
+    actions = [mark_as_complete, cancel]
+
+    def response_change(self, request: HttpRequest, obj: Booking) -> HttpResponse:
+        if "_cancel" in request.POST:
+            obj.cancel(True)
+            self.message_user(request, "This booking has been cancelled")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)

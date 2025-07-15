ee482f44b9ea2c123643792913ec32c3b714a880
Theo Billardey <toto.billardey@gmail.com>
2025-07-15 19:30:32 +0200

    feat(booking): cancel a booking
    Booking cancellation from customer or staff

--- booking/views.py
+++ booking/views.py
@@ -1,19 +1,26 @@
 from typing import Any, cast
 
 from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import BaseSerializer
+from rest_framework.status import HTTP_200_OK
+from rest_framework.viewsets import GenericViewSet
 
 from user.models import CustomUser
 
+from .enums import BookingStatus
+from .errors import BOOKING_ALREADY_CANCELED_ERROR, BOOKING_CANCEL_COMPLETED_ERROR
 from .models import Booking
+from .permissions import IsBookingOwner
 from .serializers import BookingSerializer
 
 
-class BookingView(ListAPIView, CreateAPIView):
+class BookingViewSet(CreateAPIView, ListAPIView, GenericViewSet):
     """
     List all bookings related to a customer or create Bookings
     """
@@ -32,7 +39,7 @@ class BookingView(ListAPIView, CreateAPIView):
         """List all bookings related to a customer"""
 
         self.permission_classes = [IsAuthenticated]
-        self.check_permissions(self.request)
+        self.check_permissions(request)
         return self.list(request, *args, **kwargs)
 
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
@@ -46,3 +53,30 @@ class BookingView(ListAPIView, CreateAPIView):
     def perform_create(self, serializer: BaseSerializer) -> None:
         booking: Booking = serializer.save()
         booking.send_confirmation_email()
+
+    @action(detail=True, methods=["post"])
+    def cancel(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        """Cancel a booking"""
+
+        # Check permissions
+        self.permission_classes = [IsBookingOwner]
+        booking = get_object_or_404(Booking, pk=kwargs["pk"])
+        self.check_object_permissions(request, booking)
+
+        # Validation
+        if booking.status == BookingStatus.COMPLETED:
+            raise BOOKING_CANCEL_COMPLETED_ERROR
+        if booking.status == (
+            BookingStatus.CANCELED_BY_CUSTOMER or BookingStatus.CANCELED_BY_STAFF
+        ):
+            raise BOOKING_ALREADY_CANCELED_ERROR
+        booking.cancel()
+
+        # Send cancellation email
+        if (
+            booking.status is BookingStatus.CANCELED_BY_CUSTOMER
+            or booking.status is BookingStatus.CANCELED_BY_STAFF
+        ):
+            booking.send_cancellation_email()
+        booking_serializer = BookingSerializer(booking)
+        return Response(booking_serializer.data, status=HTTP_200_OK)

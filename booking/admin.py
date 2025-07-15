86edc178b385b084cecdfb2336a1c6015b359952
Theo Billardey <toto.billardey@gmail.com>
2025-07-15 13:31:45 +0200

    feat(booking): register mark as complete admin action

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
 
+from .enums import BookingStatus
 from .models import Booking
+
+
+@admin.action(description="Mark bookings as complete")
+def mark_as_complete(modeladmin, request, queryset: QuerySet[Booking]):
+    queryset.update(status=BookingStatus.COMPLETED)
 
 
 @admin.register(Booking)
@@ -12,3 +19,4 @@ class BookingAdmin(admin.ModelAdmin):
     )
     list_filter = ("customer", "car")
     list_per_page = 30
+    actions = [mark_as_complete]

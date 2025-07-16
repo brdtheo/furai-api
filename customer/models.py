501db75fab86bd418e55f3c42678ebc2b9e02b6e
Theo Billardey <toto.billardey@gmail.com>
2025-07-16 19:40:49 +0200

    feat(customer): sync customer data with Stripe
    Populate fields when creating or updating a customer instance

--- customer/models.py
+++ customer/models.py
@@ -1,19 +1,50 @@
-from typing import Any, override
+import os
+from typing import Any, cast, override
 
-from django.db import models
+import stripe
+from django.db import IntegrityError, models
 from django.forms import ValidationError
 from django.utils import timezone
 from django_countries.fields import CountryField
 
 from user.models import CustomUser
 
+stripe.api_key = os.getenv("STRIPE_API_KEY")
+
+
+class CustomerManager(models.Manager):
+    def create(self, **kwargs: Any) -> Any:
+        if not kwargs.get("user"):
+            raise IntegrityError("Cannot create a customer without a user")
+        # Automatically create a Stripe customer and link it to created customer
+        stripe_customer = stripe.Customer.create(
+            name=f"{kwargs['first_name']} {kwargs['last_name']}",
+            email=kwargs["user"].email,
+            address={
+                "city": kwargs.get("address_city", ""),
+                "country": kwargs.get("address_country", ""),
+                "line1": kwargs.get("address_line1", ""),
+                "line2": kwargs.get("address_line2", ""),
+                "postal_code": kwargs.get("address_postal_code", ""),
+                "state": kwargs.get("address_state", ""),
+            },
+            phone=kwargs["phone"],
+        )
+        return super().create(
+            **kwargs,
+            stripe_id=stripe_customer.id,
+        )
+
 
 class Customer(models.Model):
     """Representation of a Customer"""
 
+    objects = CustomerManager()
+
     stripe_id = models.CharField(
         help_text="The Stripe customer identifier linked to the customer",
         db_comment="The Stripe customer identifier linked to the customer",
+        blank=True,
     )
     user = models.OneToOneField(
         CustomUser,
@@ -102,4 +133,21 @@ class Customer(models.Model):
     @override
     def save(self, *args: Any, **kwargs: Any) -> None:
         self.validate_passport()
+
+        if self.pk:
+            # Update associated Stripe customer
+            stripe.Customer.modify(
+                self.stripe_id,
+                name=f"{self.first_name} {self.last_name}",
+                email=self.user.email,
+                address={
+                    "city": cast(str, self.address_city),
+                    "country": cast(str, self.address_country),
+                    "line1": cast(str, self.address_line1),
+                    "line2": cast(str, self.address_line2),
+                    "postal_code": cast(str, self.address_postal_code),
+                    "state": cast(str, self.address_state),
+                },
+                phone=self.phone,
+            )
         super().save(*args, **kwargs)

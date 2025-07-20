1a1b89f9bfb57b4022971189178f7b9a22282ccc
Theo Billardey <toto.billardey@gmail.com>
2025-07-20 19:15:22 +0200

    feat(customer): exclude stripe id field in serializer

--- customer/serializers.py
+++ customer/serializers.py
@@ -6,4 +6,18 @@ from .models import Customer
 class CustomerSerializer(ModelSerializer):
     class Meta:
         model = Customer
-        fields = "__all__"
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "address_line1",
+            "address_line2",
+            "address_city",
+            "address_postal_code",
+            "address_state",
+            "address_country",
+            "phone",
+            "passport",
+            "created_at",
+            "user",
+        ]

b13692e05ff343939518e31a8711ed33bd7d38ab
Theo Billardey <toto.billardey@gmail.com>
2025-07-20 19:14:54 +0200

    refactor(customer): use viewsets

--- customer/views.py
+++ customer/views.py
@@ -1,29 +1,28 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import RetrieveAPIView, UpdateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.viewsets import GenericViewSet
 
 from .models import Customer
 from .permissions import IsCustomerUser
 from .serializers import CustomerSerializer
 
 
-class CustomerMe(RetrieveAPIView):
+class CustomerViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     """
-    Retrieve the customer instance from current authenticated user
-    """
-
-    serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self) -> Customer:
-        return get_object_or_404(Customer, user=self.request.user)
-
-
-class CustomerDetailView(UpdateAPIView):
-    """
-    Update a given customer instance
+    Retrieve or update a customer instance
     """
 
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsCustomerUser]
+
+    @action(detail=False)
+    def me(self, request: Request, *args: str, **kwargs: str) -> Response:
+        """Retrieve the customer instance from current authenticated user"""
+        customer = get_object_or_404(Customer, user=self.request.user)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data, status=HTTP_200_OK)

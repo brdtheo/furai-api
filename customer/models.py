9bea8e20808d6b7f6ce833c770f9078f41ddffc4
Theo Billardey <toto.billardey@gmail.com>
2025-07-21 22:33:57 +0200

    feat(customer): use service layer

--- customer/models.py
+++ customer/models.py
@@ -1,39 +1,26 @@
-import os
-from typing import Any, cast, override
+from typing import Any
 
-import stripe
-from django.db import IntegrityError, models
-from django.forms import ValidationError
+from django.db import models
 from django_countries.fields import CountryField
 
 from furai.models import BaseModel
 from user.models import CustomUser
 
-stripe.api_key = os.getenv("STRIPE_API_KEY")
-
 
 class CustomerManager(models.Manager):
     def create(self, **kwargs: Any) -> Any:
-        if not kwargs.get("user"):
-            raise IntegrityError("Cannot create a customer without a user")
-        # Automatically create a Stripe customer and link it to created customer
-        stripe_customer = stripe.Customer.create(
-            name=f"{kwargs['first_name']} {kwargs['last_name']}",
-            email=kwargs["user"].email,
-            address={
-                "city": kwargs.get("address_city", ""),
-                "country": kwargs.get("address_country", ""),
-                "line1": kwargs.get("address_line1", ""),
-                "line2": kwargs.get("address_line2", ""),
-                "postal_code": kwargs.get("address_postal_code", ""),
-                "state": kwargs.get("address_state", ""),
-            },
-            phone=kwargs["phone"],
-        )
-        return super().create(
-            **kwargs,
-            stripe_id=stripe_customer.id,
-        )
+        from .services import CustomerService
+
+        service = CustomerService(**kwargs)
+        customer = service.create()
+        return customer
+
+    def update(self, **kwargs: Any) -> Any:
+        from .services import CustomerService
+
+        service = CustomerService(**kwargs)
+        customer = service.update()
+        return customer
 
 
 class Customer(BaseModel):
@@ -114,35 +101,3 @@ class Customer(BaseModel):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
-
-    def validate_passport(self) -> None:
-        """Ensures the passport field is required if customer's country is not Thailand"""
-
-        if self.address_country.code != "TH" and not self.passport:
-            raise ValidationError("Passport number is required for foreign nationals")
-        pass
-
-    def clean(self) -> None:
-        self.validate_passport()
-
-    @override
-    def save(self, *args: Any, **kwargs: Any) -> None:
-        self.validate_passport()
-
-        if self.pk:
-            # Update associated Stripe customer
-            stripe.Customer.modify(
-                self.stripe_id,
-                name=f"{self.first_name} {self.last_name}",
-                email=self.user.email,
-                address={
-                    "city": cast(str, self.address_city),
-                    "country": cast(str, self.address_country),
-                    "line1": cast(str, self.address_line1),
-                    "line2": cast(str, self.address_line2),
-                    "postal_code": cast(str, self.address_postal_code),
-                    "state": cast(str, self.address_state),
-                },
-                phone=self.phone,
-            )
-        super().save(*args, **kwargs)
